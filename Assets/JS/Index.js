3dd484c8e392298b413bcd289e1351204740769a
eliab256 <bordonielia96@gmail.com>
2024-09-06 14:57:49 +0200

    aggiunta funzioni ac

--- Assets/JS/Index.js
+++ Assets/JS/Index.js
@@ -1,14 +1,16 @@
 let NumberComponing = "";
+let PartialNumber = "";
 
 //assegnazione const per ogni button HTML
 const numberButtons = document.querySelectorAll(".number");
 const dotButton = document.querySelector("#dot");
 const operatorButtons = document.querySelectorAll(".operator");
 const equalButton = document.querySelector("#equal");
-const cancelButtons = document.querySelector("#allClear");
+const cancelButton = document.querySelector("#allClear");
 
 //assegnazione della variabile JS al valore del risultato sul display HTML
 document.querySelector("#result").value = NumberComponing;
+document.querySelector("#partialResul").value = PartialNumber;
 
 //funzione che manda a schermo i numeri quando i relativi button vengono cliccati
 function NumberButtonClick(event){
@@ -18,20 +20,26 @@ function NumberButtonClick(event){
 }
 
 function OperatorButtonClick(event){
-    if(!NumberComponing && event.target.value === "-"){
+    //permetto di scrivere valori negativi mettendo il - all' inizio e non consentendo agli altri operatori di essere scritti se non c'è un numero
+    if(!NumberComponing && event.target.value === "-"){   
         NumberComponing = event.target.value;
         document.querySelector("#result").value = NumberComponing;
     } 
-    if(NumberComponing){
-        NumberComponing += event.target.value;
-        document.querySelector("#Result").value = NumberComponing;
+    //permetto di usare gli altri operatori se ho un numero già scritto
+    if(NumberComponing && NumberComponing != "-"){       
+        PartialNumber = NumberComponing + event.target.value;
+        document.querySelector("#partialResul").value = PartialNumber;
+        NumberComponing = "";
+        document.querySelector("#result").value = NumberComponing;
     }
-
-    
-
 }
 
-
+function CancelButtonClick(event){
+    NumberComponing = "";
+    PartialNumber = "";
+    document.querySelector("#result").value = NumberComponing;
+    document.querySelector("#partialResul").value = PartialNumber;
+}
 
 
 numberButtons.forEach(button => {
@@ -41,3 +49,5 @@ numberButtons.forEach(button => {
 operatorButtons.forEach(button => {
     button.addEventListener('click', OperatorButtonClick);
 });
+
+cancelButton.addEventListener('click', CancelButtonClick);
