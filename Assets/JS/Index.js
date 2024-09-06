2983f9024e15cbaa00cf4de175ea232365baa544
eliab256 <bordonielia96@gmail.com>
2024-09-06 16:18:29 +0200

    calcolatrice completata

--- Assets/JS/Index.js
+++ Assets/JS/Index.js
@@ -12,29 +12,74 @@ const cancelButton = document.querySelector("#allClear");
 document.querySelector("#result").value = NumberComponing;
 document.querySelector("#partialResul").value = PartialNumber;
 
+//Funzioni innescate dal click di un pulsante
+
 //funzione che manda a schermo i numeri quando i relativi button vengono cliccati
 function NumberButtonClick(event){
+    if(typeof NumberComponing === "number"){
+        CalculatorReset();
+    }
     NumberComponing += event.target.value;
-
     document.querySelector("#result").value = NumberComponing;
 }
 
+//funzione che aggiunge il punto per ottenere valori decimali
+function DotButtonClick(event){
+    if(typeof NumberComponing === "number"){
+        CalculatorReset();
+    }
+    NumberComponing += event.target.value;
+    document.querySelector("#result").value = NumberComponing;
+}
+
+//funzione che permette di aggiungere gli operatori per fare i calcoli
 function OperatorButtonClick(event){
+    if(typeof NumberComponing === "number"){
+        CalculatorReset();
+    }
     //permetto di scrivere valori negativi mettendo il - all' inizio e non consentendo agli altri operatori di essere scritti se non c'è un numero
     if(!NumberComponing && event.target.value === "-"){   
         NumberComponing = event.target.value;
         document.querySelector("#result").value = NumberComponing;
     } 
     //permetto di usare gli altri operatori se ho un numero già scritto
-    if(NumberComponing && NumberComponing != "-"){       
-        PartialNumber = NumberComponing + event.target.value;
+    else if(NumberComponing && NumberComponing !== "-"){       
+        PartialNumber += NumberComponing + event.target.value;
         document.querySelector("#partialResul").value = PartialNumber;
         NumberComponing = "";
         document.querySelector("#result").value = NumberComponing;
     }
+    //alert in caso si selezione un operatore senza avere numeri 
+    else if(!NumberComponing && event.target.value != "-"){
+        alert("Se non hai digitato alcun numero l' unico operatore disponibile è il - per ottenere numeri negativi")
+    }
 }
 
-function CancelButtonClick(event){
+//funzione che restituisce il risultato di tutta l' operazione
+function EqualButtonClick(event){
+    if(NumberComponing || PartialNumber){
+        if(!NumberComponing){
+            NumberComponing = "0";
+        }
+        let finalResult = eval(PartialNumber + NumberComponing);
+        NumberComponing = finalResult;
+        PartialNumber = "";
+        document.querySelector("#result").value = NumberComponing;
+        document.querySelector("#partialResul").value = PartialNumber;
+    } else{
+        alert("non ci sono operazioni da eseguire")
+    }
+}
+
+//funzione che cancella tutti i valori a schermo
+function CancelButtonClick(event){  
+    NumberComponing = "";
+    PartialNumber = "";
+    document.querySelector("#result").value = NumberComponing;
+    document.querySelector("#partialResul").value = PartialNumber;
+}
+
+function CalculatorReset(){
     NumberComponing = "";
     PartialNumber = "";
     document.querySelector("#result").value = NumberComponing;
@@ -42,12 +87,19 @@ function CancelButtonClick(event){
 }
 
 
+
+//Funzioni trigger click
 numberButtons.forEach(button => {
     button.addEventListener('click', NumberButtonClick);
 });
+
+dotButton.addEventListener('click', DotButtonClick);
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', OperatorButtonClick);
 });
 
+equalButton.addEventListener('click', EqualButtonClick);
+
 cancelButton.addEventListener('click', CancelButtonClick);
+
